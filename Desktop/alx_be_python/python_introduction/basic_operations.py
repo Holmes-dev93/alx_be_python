7203df9352f064d36119f9068339d1b2c656d984
holmesburton <holmesburton7@gmail.com>
2025-05-26 09:31:45 +0200

    Add basic_operations.py and ensure all mandatory task files are included

---
+++ Desktop/alx_be_python/python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Assign specific values to number1 and number2
+number1 = 10
+number2 = 5
+
+# Perform addition
+addition_result = number1 + number2
+
+# Perform subtraction
+subtraction_result = number1 - number2
+
+# Perform multiplication
+multiplication_result = number1 * number2
+
+# Print the results in the specified format
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
